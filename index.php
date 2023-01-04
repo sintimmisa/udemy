8ca766b27b7cdcda7b4cd014aa7c6b78fb736ab0
Godfred D. Sintim-Misah <sintim.misa@gmail.com>
2023-01-04 23:15:28 +0000

    Initial Commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php  bloginfo('charset');  ?>">
+   <?php wp_head();?>
+    
+</head>
+<body <?php body_class('example'); ?>>
+<?php wp_body_?>
+    <h2>Smaple Post!<h2>
+        <p>This is sample post</p>
+    
+  <?php wp_footer();?>
+</body> 
+</html>
